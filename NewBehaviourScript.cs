6d5ff12514e33069312a6c52651b498a20894c00
tantrungnguyen01 <110245526+tantrungnguyen01@users.noreply.github.com>
2023-09-11 02:08:57 +0700

    Update 11/9/2023

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -9,34 +9,75 @@ using TMPro;
 using UnityEngine.UI;
 using static NewBehaviourScript;
 using Unity.VisualScripting;
+using UnityEngine.U2D;
 
 
 public class NewBehaviourScript : MonoBehaviour 
 {
-   
+    
+
     WebSocket websocket;
     //bool isConnectClose = true;
     public Text txtSmall;
     public Text txtBig;
     public Text txtTime;
     public Text txt_id;
+    public Text txtCountDown;
+    
     //public Text txtResult;
     public GameObject dice;
+
+    public GameObject Blow;
+    
+
+    public SpriteAtlas spriteAtlas;
 
 
     public Animator animatorXiu;
     public Animator animatorTai;
 
+    
 
 
+    IEnumerator ShowResultCoroutine()
+    {
+        float thoigianbatdau = Time.time;
+        float thoigiandichuyen = 0.4f;
+
+        Vector3 startPos = Blow.transform.position; 
+        Vector3 endPos = startPos + new Vector3(0.5f, 1f, 0.5f); 
+
+        while (Time.time - thoigianbatdau < thoigiandichuyen)
+        {
+            
+            float lerpTime = (Time.time - thoigianbatdau) / thoigiandichuyen;
+
+           
+            Blow.transform.position = Vector3.MoveTowards(startPos, endPos, lerpTime);
+
+            yield return null;
+
+        }
 
 
+        while (Time.time - thoigianbatdau < thoigiandichuyen * 2)
+        {
+            float lerpTime = (Time.time - thoigianbatdau - thoigiandichuyen) / thoigiandichuyen;
+
+            Blow.transform.position = Vector3.Lerp(endPos, startPos, lerpTime);
+
+            yield return null;
+            Blow.SetActive(false);
+        }
+
+        Blow.SetActive(false);
+    }
 
     // Start is called before the first frame update
     async void Start()
     {
+   
 
-        
         dice.SetActive(false);
        
         websocket = new WebSocket("ws://localhost:8080/");
@@ -71,32 +112,60 @@ public class NewBehaviourScript : MonoBehaviour
 
             // getting the message as a string
              var message = System.Text.Encoding.UTF8.GetString(bytes);
-             //Debug.Log("OnMessage!-SERVER: " + message);
+            //Debug.Log("OnMessage!-SERVER: " + message);
+
             RoundData roundData = JsonUtility.FromJson<RoundData>(message);
-            txtSmall.GetComponent<Text>().text = roundData.small_money.ToString();
-            txtBig.GetComponent<Text>().text = roundData.big_money.ToString();
-            txtTime.GetComponent<Text>().text = (--roundData.counter).ToString("00");
-            txt_id.GetComponent<Text>().text = "#" + roundData._id.ToString();
-            //txtResult.GetComponent<Text>().text = roundData.result.ToString();
-            
-
-            if (roundData.counter == 10)
+            if (roundData.countdown > 0)
             {
-
-                dice.SetActive(true);
+                txtCountDown.GetComponent<Text>().text = roundData.countdown.ToString();
             }
-            else
-            {
-                dice.SetActive(false);
-            }
+            else {
 
-            if (roundData.result == 0) {
+                txtSmall.GetComponent<Text>().text = roundData.small_money.ToString();
+                txtBig.GetComponent<Text>().text = roundData.big_money.ToString();
+                txtTime.GetComponent<Text>().text = (--roundData.counter).ToString("00");
+                txt_id.GetComponent<Text>().text = "#" + roundData._id.ToString();
+                //txtResult.GetComponent<Text>().text = roundData.result.ToString();
 
 
-                animatorXiu.SetTrigger("Animation_connect_Xiu");
 
-            }else if (roundData.result == 1){
-                animatorTai.SetTrigger("Animation_connect_Tai");
+                SpriteRenderer diceImage = dice.GetComponent<SpriteRenderer>();
+
+                if (roundData.counter == 10)
+                {
+                    dice.SetActive(true);
+                    txtTime.enabled = false;
+                    txtCountDown.enabled = true;
+
+                    int diceResult = roundData.dice; 
+                    Sprite diceSprite = spriteAtlas.GetSprite("dice-" + diceResult);
+                    diceImage.sprite = diceSprite;
+
+
+                    StartCoroutine(ShowResultCoroutine());
+                }
+                else
+                {
+                    txtTime.enabled=true;
+                    dice.SetActive(false);
+                    txtCountDown.enabled = false;
+                    Blow.SetActive(true);
+                   
+                }
+
+
+                if (roundData.result == 0)
+                {
+
+
+                    animatorXiu.SetTrigger("Animation_connect_Xiu");
+
+                }
+                else if (roundData.result == 1)
+                {
+                    animatorTai.SetTrigger("Animation_connect_Tai");
+                }
+
             }
 
 
@@ -114,6 +183,7 @@ public class NewBehaviourScript : MonoBehaviour
        
 
     }
+    
 
     // Update is called once per frame
     void Update()
@@ -140,6 +210,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         await websocket.Close();
     }
+
     //async void Reconnect()
     //{
     //    await Task.Delay(5000); // Delay for 5 seconds before reconnecting
@@ -156,6 +227,8 @@ public class NewBehaviourScript : MonoBehaviour
         public int counter;
         public int result;
         public int dice;
+        public int countdown;
+       
     }
 
 
